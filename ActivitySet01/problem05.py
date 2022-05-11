68e07854f046c53fb5c89aa329123bd3eb955dee
Shivanshu <96230206+Shivanshu0309@users.noreply.github.com>
2022-05-11 12:37:11 +0000

    code

--- ActivitySet01/problem05.py
+++ ActivitySet01/problem05.py
@@ -2,11 +2,20 @@
 
 
 def computepay(h, r):
-    pass  # ...
+    if h > 40:
+        x = h*r
+        o = (h - 40)*0.5*r
+        p = x+o
+        return p 
+    else:
+        x = h*r
+        return x 
+      
+    
 
-
-hrs = float(input("Enter hours? "))
-rte = float(input("Enter rate per hour? "))
-
-p = computepay(hrs, rte)
+hrs = input("Enter Hours:")
+h = float(hrs)
+rte = input("enter rate per hour?")
+r = float(rte)
+p = computepay(h,r)
 print("Pay", p)
