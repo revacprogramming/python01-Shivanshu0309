ffe3b1f91c2e8a284765721b8cab187d577a5dc2
Shivanshu <96230206+Shivanshu0309@users.noreply.github.com>
2022-06-08 09:39:43 +0000

    Code

--- ActivitySet01/problem09.py
+++ ActivitySet01/problem09.py
@@ -1,3 +1,15 @@
 # Lists
 
 filename = "dataset/romeo.txt"
+fname = input("Enter file name: ")
+fh = open(fname)
+lst = list()
+for line in fh:
+    for word in line.split():
+        if word in lst:
+            continue
+        else:
+            lst.append(word)
+lst.sort()   
+    
+print(lst)
---
+++ ActivitySet01/problem18.py
@@ -0,0 +1,7 @@
+a ' reva' 
+c = 0
+for i in a :
+  print (i)
+  if i == a :
+      c = c+1
+print (c)
